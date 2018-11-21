28069c9c0fc1551827aebc5fc0aa31497877a16c
Adrian Mark Perea <aperea@freelancer.com>
2018-11-21 16:46:32 +0800

    Add default app configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <h1>FLN University</h1> 
+  `
 })
 export class AppComponent {
-  title = 'fln-university-front-end';
 }
